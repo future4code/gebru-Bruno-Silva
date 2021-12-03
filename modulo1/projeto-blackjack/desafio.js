fb701467958c274df70b0c00cb0b3b75824f17b9
Bruno Moura <89053281+obrunomoura@users.noreply.github.com>
2021-12-03 19:51:58 -0300

    adicionei-os-arquivos-do-projeto-blackjack

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -10,30 +10,3 @@
  * 
  * 
  */
-
-    console.log("Boas vindas ao jogo de Blackjack!")
-    confirm("Quer iniciar uma nova rodada?")
-
-      const cartaUsuario1 = comprarCarta()
-      const cartaUsuario2 = comprarCarta()
-      const cartaComputador1 = comprarCarta()
-      const cartaComputador2 = comprarCarta()
-      
-      const somaCartaUsuario = Number(cartaUsuario1.valor) + Number(cartaUsuario2.valor)
-      const somaCartaComputador = Number(cartaComputador1.valor) + Number(cartaComputador2.valor)
-      
-      console.log("Usuário - ", cartaUsuario1.texto, cartaUsuario2.texto, "-", somaCartaUsuario)
-      console.log("Computador - ", cartaComputador1.texto, cartaComputador2.texto, "-", somaCartaComputador)
-
-   if (somaCartaUsuario > somaCartaComputador && somaCartaUsuario <= 21) {
-      console.log("O usuário ganhou!")
-   }
-   else if (somaCartaComputador > somaCartaUsuario && somaCartaComputador <= 21) {
-      console.log("O computador ganhou!")
-   }
-   else if (somaCartaComputador === somaCartaUsuario) {
-      console.log("Empate!")
-   }
-   else {
-      console.log("O jogo acabou")
-   }
