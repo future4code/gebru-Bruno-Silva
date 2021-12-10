23386ccd0ea8be16e72fd7aec32401cf6f358b56
Bruno Moura <89053281+obrunomoura@users.noreply.github.com>
2021-12-09 21:45:45 -0300

    exercicio-08-pronto

--- modulo1/semana06/revisao-js/exercicios.js
+++ modulo1/semana06/revisao-js/exercicios.js
@@ -62,7 +62,13 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   let array = []
+   for(let i = 0; array.length < n; i++) {
+        if(i % 2 === 0) {
+            array.push(i)
+        }
+   }
+   return array
 }
 
 // EXERCÍCIO 09
