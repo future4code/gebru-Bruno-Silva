bf63c019f4d6800f0005ca3d004e78b4c873e6da
Bruno Moura <89053281+obrunomoura@users.noreply.github.com>
2022-05-27 21:09:10 -0300

    commit inicial

---
+++ modulo5/lista-typescript/src/exercicio1.ts
@@ -0,0 +1,9 @@
+// function frase(name:string, date:string):string {
+    const nome = "Bruno"
+    const data = "5/setembro/1995"
+
+    const divisao = data.split("/")
+
+   console.log(`Olá me chamo ${nome}, nasci no dia ${divisao[0]} do mês de ${divisao[1]} do ano de ${divisao[2]}`)
+
+// }
