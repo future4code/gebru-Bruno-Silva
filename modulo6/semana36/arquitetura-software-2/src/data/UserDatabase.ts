f5bdcc164e087e895f9cac9f65c65191ef8af45d
Bruno Moura <89053281+obrunomoura@users.noreply.github.com>
2022-07-28 21:06:56 -0300

    exercício

---
+++ modulo6/semana36/arquitetura-software-2/src/data/UserDatabase.ts
@@ -0,0 +1,16 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+  private static TABLE_NAME = "LABEFLIX_USER";
+
+  async create({ id, name, email, password }: any): Promise<void> {
+    await UserDatabase.connection
+      .insert({
+        id,
+        name,
+        email,
+        password,
+      })
+      .into(UserDatabase.TABLE_NAME);
+  }
+}
