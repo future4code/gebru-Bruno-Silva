4b32d30049381a2e0596af1be0eaff17075bdf6c
Bruno Moura <89053281+obrunomoura@users.noreply.github.com>
2022-08-02 19:22:55 -0300

    exercício arquitetura-software-3

---
+++ modulo6/semana37/arquitetura-software-3/src/business/UserBusiness.ts
@@ -0,0 +1,31 @@
+import { UserDatabase } from "../data/UserDatabase";
+
+
+export class UserBusiness {
+public createUser = async (input: any) => {
+   try {
+     const { name, nickname, email, password } = input;
+ 
+     if (!name || !nickname || !email || !password) {
+       throw new Error(
+         'Preencha os campos "name","nickname", "email" e "password"'
+       );
+     }
+ 
+     const id: string = Date.now().toString()
+ 
+     const userDatabase = new UserDatabase();
+     await userDatabase.insertUser({
+       id,
+       name,
+       nickname,
+       email,
+       password,
+     });
+   } catch (error: any) {
+     throw new Error(error.message);
+   }
+ };
+
+}
+
